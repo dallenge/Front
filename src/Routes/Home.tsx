0b8f3b756a85d59b5179f6aca82ca50883d7caf1
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-01-09 18:37:42 +0900

    💄 Design : Home 페이지 UI 세 구간으로 나눔

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Card = styled.div<{ backgroundColor: string }>`
+  height: 90vh;
+  background-color: ${(props) => props.backgroundColor};
+`;
 function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Card backgroundColor="white">1</Card>
+      <Card backgroundColor="var(--color-white)">2</Card>
+      <Card backgroundColor="white">3</Card>
+    </div>
+  );
 }
 
 export default Home;
