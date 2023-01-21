4fa86fee5af3465ec94a38bff9b03d1ed24e3b40
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-01-21 15:09:22 +0900

    💄 Design : 메인페이지 UI 수정

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,32 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Card = styled.div<{ backgroundColor: string; height: string }>`
+const Card = styled.div<{ background: string; height: string }>`
   width: 100%;
   height: ${(props) => props.height};
-  background-color: ${(props) => props.backgroundColor};
+  background: ${(props) => props.background};
   padding: 20px;
   display: flex;
   justify-content: flex-start;
+  background-size: cover;
 `;
-
-const PopularChallenge = styled.div`
-  height: 400px;
-  width: 18rem;
-  border: 1px solid var(--color-blue);
-  background: var(--color-sky);
-`;
-
 function Home() {
   return (
     <div>
-      <Card backgroundColor="white" height="90vh">
-        1
+      <Card background="url('/main.jpg')" height="80vh">
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            margin: '150px',
+            color: 'black',
+            fontSize: '70px',
+          }}
+        >
+          <div>타인에게 공유하는</div>
+          <div>나만의 챌린지</div>
+          <div style={{ fontSize: '20px', color: 'black', fontWeight: 'bold' }}>나만의 데일리 챌린지를</div>
+          <div style={{ fontSize: '20px', color: 'black', fontWeight: 'bold' }}>사람들에게 공유하세요!</div>
+        </div>
       </Card>
-      <Card backgroundColor="var(--color-white)" height="90vh">
-        2
+      <Card background="var(--color-white)" height="50vh">
+        카테고리 넣을 위치입니다.
       </Card>
-      <Card backgroundColor="white" height="600px">
+      <Card background="white" height="600px">
         <div style={{ width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
           <div style={{ fontSize: '20px', fontWeight: '600' }}>인기 챌린지</div>
           <div style={{ width: '100%', display: 'flex', marginTop: '50px', justifyContent: 'space-around' }}>
@@ -37,6 +44,25 @@ function Home() {
           </div>
         </div>
       </Card>
+    </div>
+  );
+}
+function PopularChallenge() {
+  return (
+    <div
+      style={{
+        height: '400px',
+        width: '300px',
+        border: ' 1px solid black',
+        background: ' var(--color-sky)',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        padding: '10px',
+      }}
+    >
+      <div style={{ height: '200px', width: '280px', background: 'white', border: '1px solid black' }}></div>
+      <div style={{ marginTop: '20px' }}>인기 챌린지 입니다.</div>
     </div>
   );
 }
