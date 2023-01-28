5b6e9b839feb9be2669cfed0ab1c8254e08ca9d6
Doeunnkimm <112946860+Doeunnkimm@users.noreply.github.com>
2023-01-28 16:43:52 +0900

    :lipstick: Design : 진행 챌린지 편집 컴포넌트 추가 및 라우팅

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import CONSTANT_INFO from '../Constant/Constant';
 import Searchbar from './Searchbar';
