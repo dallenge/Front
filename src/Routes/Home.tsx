bbc3d1eee6fb30756827d1bda91dd5673e4f17de
Doeunnkimm <doeunnkimm@gmail.com>
2023-02-24 21:01:23 +0900

    :lipstick: Design : Home - 스크롤 애니메이션 추가

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -5,66 +5,9 @@ import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FaMedal } from 'react-icons/fa';
-const Card = styled.div<{ background: string; height: string }>`
-  width: 100%;
-  height: ${(props) => props.height};
-  background: ${(props) => props.background};
-  padding: 20px;
-  display: flex;
-  justify-content: flex-start;
-  background-size: cover;
-`;
-const CategoryBtn = styled.div`
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #f0ecec;
-  font-weight: bold;
-  font-size: 20px;
-  cursor: pointer;
-  &:hover {
-    background: var(--color-sky);
-    font-size: 25px;
-  }
-`;
-
-const Challenge = styled.div`
-  width: 300px;
-  height: 300px;
-  background: var(--color-sky);
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  position: relative;
-  border-radius: 10px;
-  :hover {
-    cursor: pointer;
-  }
-`;
+import useScrollFadeIn from '../Hooks/useScrollFadeIn';
 
 function Home() {
-  type Challenge = {
-    id: number;
-    title: string;
-    challengeCategory: string;
-    challengeLocation: string;
-    challengeDuration: string;
-    howManyUsersAreInThisChallenge: number;
-    challengeOwnerUser: ChallengeOwnerUser;
-    challengeImgUrls: string;
-    created_at: string;
-  };
-
-  type ChallengeOwnerUser = {
-    userName: string;
-    userId: number;
-    email: string;
-  };
-
   const Category = ['공부', '봉사', '운동', '경제', '건강'];
   const medal = ['gold', 'silver', '#964b00'];
   const URL = process.env.REACT_APP_URL;
@@ -85,6 +28,15 @@ function Home() {
     let day = date.split(' ')[0];
     let time = date.split(' ')[1].slice(0, 8);
     return day + ' ' + time;
+  };
+
+  const animatedItem: any = {
+    0: useScrollFadeIn('up', 0, 0),
+    1: useScrollFadeIn('up', 1, 0.2),
+    2: useScrollFadeIn('up', 1, 0.3),
+    3: useScrollFadeIn('up', 1, 0.4),
+    4: useScrollFadeIn('up', 1, 0.5),
+    5: useScrollFadeIn('up', 1, 0.6),
   };
 
   useEffect(() => {
@@ -109,19 +61,21 @@ function Home() {
           <div style={{ fontSize: '20px', color: 'black', fontWeight: 'bold' }}>사람들에게 공유하세요!</div>
         </div>
       </Card>
-      <Card background="white" height="50vh">
-        <div style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-between' }}>
-          {Category.map((category) => {
-            return (
-              <CategoryBtn
-                onClick={() => {
-                  navigate(`/challengelist//${category}`);
-                }}
-              >
-                {category}
-              </CategoryBtn>
-            );
-          })}
+      <Card background="white" height="50vh" {...animatedItem[0]}>
+        <div
+          {...animatedItem[0]}
+          style={{ display: 'flex', width: '100%', alignItems: 'center', justifyContent: 'space-between' }}
+        >
+          {Category.map((category, index) => (
+            <CategoryBtn
+              {...animatedItem[index + 1]}
+              onClick={() => {
+                navigate(`/challengelist//${category}`);
+              }}
+            >
+              {category}
+            </CategoryBtn>
+          ))}
         </div>
       </Card>
       <Card background="var(--color-white)" height="600px">
@@ -179,3 +133,62 @@ function Home() {
 }
 
 export default Home;
+
+type Challenge = {
+  id: number;
+  title: string;
+  challengeCategory: string;
+  challengeLocation: string;
+  challengeDuration: string;
+  howManyUsersAreInThisChallenge: number;
+  challengeOwnerUser: ChallengeOwnerUser;
+  challengeImgUrls: string;
+  created_at: string;
+};
+
+type ChallengeOwnerUser = {
+  userName: string;
+  userId: number;
+  email: string;
+};
+
+const Card = styled.div<{ background: string; height: string }>`
+  width: 100%;
+  height: ${(props) => props.height};
+  background: ${(props) => props.background};
+  padding: 20px;
+  display: flex;
+  justify-content: flex-start;
+  background-size: cover;
+`;
+const CategoryBtn = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #f0ecec;
+  font-weight: bold;
+  font-size: 20px;
+  cursor: pointer;
+  &:hover {
+    background: var(--color-sky);
+    font-size: 25px;
+  }
+`;
+
+const Challenge = styled.div`
+  width: 300px;
+  height: 300px;
+  background: var(--color-sky);
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  position: relative;
+  border-radius: 10px;
+  :hover {
+    cursor: pointer;
+  }
+`;
