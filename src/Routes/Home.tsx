de46f18fed42110526f83d1bd3b13859996366e3
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-03-07 23:25:14 +0900

    ✨ Feat : 메인 페이지 로그인 시 오늘의 챌린지 표시

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,17 +2,94 @@ import React from 'react';
 import styled from 'styled-components';
 import PopularChallenge from '../Components/PopularChallenge';
 import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { FaMedal } from 'react-icons/fa';
 import useScrollFadeIn from '../Hooks/useScrollFadeIn';
+import CONSTANT_INFO from '../Constant/Constant';
 
 function Home() {
   const Category = ['공부', '봉사', '운동', '경제', '건강'];
   const medal = ['gold', 'silver', '#964b00'];
   const URL = process.env.REACT_APP_URL;
+
+  const IMAGE_URL = CONSTANT_INFO.IMAGE_URL;
   const navigate = useNavigate();
   const [popularChallenge, setPopularChallenge] = useState<Challenge[]>([]);
+  const [myChallengeList, setMyChallengeList] = useState<ChallengeList[]>([]);
+
+  const ChallengeBox = styled.div`
+    border: 3px solid var(--color-sky);
+    background: rgba(219, 226, 239, 0.3);
+    border-radius: 8px;
+    width: 90%;
+    height: 60px;
+    padding: 0 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 30px;
+  `;
+
+  const CursorDiv = styled.div`
+    &:hover {
+      cursor: pointer;
+    }
+  `;
+  interface ChallengeList {
+    challengeId: number;
+    challengeTitle: string;
+    challengeContent: string;
+    challengeStatus: string;
+  }
+
+  const getMyChallenge = useCallback(async () => {
+    const config = {
+      method: 'get',
+      url: `${URL}/user/participate`,
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+    };
+    await axios(config)
+      .then((res) => {
+        setMyChallengeList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  const onClickCheck = (status: string, id: number) => {
+    let config = {};
+    if (status === 'SUCCESS') {
+      config = {
+        method: 'post',
+        url: `${URL}/challenge/${id}/pause`,
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      };
+    } else {
+      config = {
+        method: 'post',
+        url: `${URL}/challenge/${id}/success`,
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+      };
+    }
+    axios(config)
+      .then(() => {
+        getMyChallenge();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getMyChallenge();
+  }, [getMyChallenge]);
 
   const getPopularChallenge = async () => {
     const config = {
@@ -44,23 +121,80 @@ function Home() {
   }, []);
   return (
     <div>
-      <Card background="url('/main.jpg')" height="90vh">
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            margin: '200px',
-            color: 'black',
-            fontSize: '70px',
-          }}
-        >
-          <div>타인에게 공유하는</div>
-          <div>나만의 챌린지</div>
-          <div style={{ fontSize: '20px', color: 'black', fontWeight: 'bold' }}>나만의 데일리 챌린지를</div>
-          <div style={{ fontSize: '20px', color: 'black', fontWeight: 'bold' }}>사람들에게 공유하세요!</div>
-        </div>
-      </Card>
+      {localStorage.getItem('token') ? (
+        <Card background="var(--color-white)" height="70vh">
+          <div
+            style={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              color: 'black',
+            }}
+          >
+            <div
+              style={{
+                fontSize: '30px',
+                fontWeight: 'bolder',
+              }}
+            >
+              ⭐️오늘의 챌린지⭐️
+            </div>
+            <div
+              style={{
+                width: '70vw',
+                height: '400px',
+                marginTop: '20px',
+                border: '2px solid var(--color-blue)',
+                background: 'white',
+                overflowY: 'auto',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                padding: '30px',
+              }}
+            >
+              {myChallengeList.map((challenge) => {
+                return (
+                  <ChallengeBox>
+                    <div style={{ fontSize: '20px' }}>{challenge.challengeTitle}</div>
+                    <CursorDiv
+                      onClick={() => {
+                        onClickCheck(challenge.challengeStatus, challenge.challengeId);
+                      }}
+                      style={{ display: 'inline-block', marginRight: '20px', marginTop: '8px' }}
+                    >
+                      {challenge.challengeStatus == 'SUCCESS' ? (
+                        <img style={{ width: '50px' }} src={IMAGE_URL.CHECK_YES_URL} />
+                      ) : (
+                        <img style={{ width: '50px' }} src={IMAGE_URL.CHECK_NO_URL} />
+                      )}
+                    </CursorDiv>
+                  </ChallengeBox>
+                );
+              })}
+            </div>
+          </div>
+        </Card>
+      ) : (
+        <Card background="url('/main.jpg')" height="90vh">
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              margin: '200px',
+              color: 'black',
+              fontSize: '70px',
+            }}
+          >
+            <div>타인에게 공유하는</div>
+            <div>나만의 챌린지</div>
+            <div style={{ fontSize: '20px', color: 'black', fontWeight: 'bold' }}>나만의 데일리 챌린지를</div>
+            <div style={{ fontSize: '20px', color: 'black', fontWeight: 'bold' }}>사람들에게 공유하세요!</div>
+          </div>
+        </Card>
+      )}
       <Card background="white" height="50vh" {...animatedItem[0]}>
         <div
           {...animatedItem[0]}
@@ -83,7 +217,6 @@ function Home() {
           <div style={{ fontSize: '20px', fontWeight: '600' }}>인기 챌린지</div>
           <div style={{ width: '100%', display: 'flex', marginTop: '50px', justifyContent: 'space-around' }}>
             {popularChallenge.map((challenge, i) => {
-              console.log(challenge);
               return (
                 <Challenge key={i} onClick={() => (window.location.href = `/challenge/${challenge.id}`)}>
                   {i <= 2 ? (
