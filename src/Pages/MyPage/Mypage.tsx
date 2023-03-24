60cb7ba995ec603475663c631b49838ee0d726ea
Sungwk <73461032+Sungwk@users.noreply.github.com>
2023-03-24 17:16:12 +0900

    🎨 Refactor : 마이페이지 컴포넌트 분리

--- src/Pages/MyPage/Mypage.tsx
+++ src/Pages/MyPage/Mypage.tsx
@@ -2,9 +2,7 @@ import axios from 'axios';
 import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Progressbar from '../../Components/Progressbar';
-import CONSTANT_INFO from '../../Constant/Constant';
-
-const IMAGE_URL = CONSTANT_INFO.IMAGE_URL;
+import ChallengeItem from './ChallengeItem';
 
 const Container = styled.div`
   text-align: left;
@@ -21,58 +19,6 @@ const Image = styled.img`
   border-radius: 50%;
 `;
 
-const ChallengeBox = styled.div`
-  border: 3px solid var(--color-sky);
-  background: rgba(219, 226, 239, 0.3);
-  border-radius: 8px;
-  width: 100%;
-  margin: 0 auto;
-  padding: 0 25px;
-  padding-top: 25px;
-  padding-bottom: 10px;
-  margin: 25px 0;
-`;
-
-const TitleDiv = styled.div`
-  font-size: 24px;
-  font-weight: 700;
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const TogleImageDown = styled.img`
-  width: 22px;
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const TogleImageUp = styled.img`
-  width: 22px;
-  transform: rotate(180deg);
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const DailyBox = styled.div`
-  width: 96%;
-  height: 110px;
-  display: flex;
-  justify-content: space-between;
-  padding: 10px;
-`;
-const DayBox = styled.div<{ background: string }>`
-  width: 100px;
-  height: 100px;
-  border-radius: 15px;
-  background: ${(props) => props.background};
-  font-size: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 interface ChallengeList {
   challengeId: number;
   challengeTitle: string;
@@ -196,6 +142,7 @@ export default function Mypage() {
                   pushChallenge={pushChallenge}
                   status={challenge.challengeStatus}
                   weeklyAchievement={challenge.weeklyAchievement}
+                  getMyChallenge={getMyChallenge}
                 />
               );
             })}
@@ -206,116 +153,3 @@ export default function Mypage() {
     </div>
   );
 }
-
-const CursorDiv = styled.div`
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const ChallengeItem = ({
-  id,
-  title,
-  content,
-  status,
-  pushChallenge,
-  weeklyAchievement,
-}: {
-  id: number;
-  title: string;
-  content: string;
-  status: string;
-  pushChallenge: any;
-  weeklyAchievement: boolean[];
-}) => {
-  const URL = process.env.REACT_APP_URL;
-  const [challengeStatus, setChallengeStatus] = useState<string>(status);
-  const [isChecked, setIsChecked] = useState<boolean>(challengeStatus === 'SUCCESS' ? true : false);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const onClickCheck = () => {
-    let config = {};
-    if (challengeStatus === 'SUCCESS') {
-      pushChallenge(id, 'delete');
-      setIsChecked(false);
-      config = {
-        method: 'post',
-        url: `${URL}/challenge/${id}/pause`,
-        headers: {
-          Authorization: 'Bearer ' + localStorage.getItem('token'),
-        },
-      };
-    } else {
-      pushChallenge(id, 'add');
-      setIsChecked(true);
-      config = {
-        method: 'post',
-        url: `${URL}/challenge/${id}/success`,
-        headers: {
-          Authorization: 'Bearer ' + localStorage.getItem('token'),
-        },
-      };
-    }
-
-    axios(config)
-      .then((res) => {
-        if (challengeStatus === 'SUCCESS') {
-          setChallengeStatus('PUASE');
-        } else {
-          setChallengeStatus('SUCCESS');
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-    setIsChecked(!isChecked);
-  };
-
-  return (
-    <div>
-      <ChallengeBox>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div style={{ display: 'inline-block' }}>
-            <TitleDiv>{title}</TitleDiv>
-            <div style={{ fontSize: '14px', marginTop: '5px', fontWeight: '600' }}>{content}</div>
-          </div>
-          <CursorDiv onClick={onClickCheck} style={{ display: 'inline-block', marginRight: '20px', marginTop: '8px' }}>
-            {isChecked ? (
-              <img style={{ width: '50px' }} src={IMAGE_URL.CHECK_YES_URL} />
-            ) : (
-              <img style={{ width: '50px' }} src={IMAGE_URL.CHECK_NO_URL} />
-            )}
-          </CursorDiv>
-        </div>
-        <div
-          onClick={() => {
-            setIsOpen(!isOpen);
-          }}
-          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
-        >
-          {isOpen ? (
-            <TogleImageDown src={IMAGE_URL.TOGLE_IMAGE_URL} />
-          ) : (
-            <TogleImageUp src={IMAGE_URL.TOGLE_IMAGE_URL} />
-          )}
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          {isOpen && <DailyInfo weeklyAchievement={weeklyAchievement} />}
-        </div>
-      </ChallengeBox>
-    </div>
-  );
-};
-
-const DailyInfo = ({ weeklyAchievement }: { weeklyAchievement: boolean[] }) => {
-  const weeksday = ['월', '화', '수', '목', '금', '토', '일'];
-  return (
-    <DailyBox>
-      {weeklyAchievement.map((achievement, i) => {
-        if (achievement) return <DayBox background="var(--color-blue)">{weeksday[i]}</DayBox>;
-        else return <DayBox background="white">{weeksday[i]}</DayBox>;
-      })}
-    </DailyBox>
-  );
-};
